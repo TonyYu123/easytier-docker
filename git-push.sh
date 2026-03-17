c58ba99ed0e256d7df0b48f0416835a7ec56bad6
MajoSissi <all.hail.lvib@gmail.com>
2026-03-17 15:31:10 +0800

    Standard Update

--- git-push.sh
+++ git-push.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# 处理Free配置
-bash config-free.sh
-# 提交 gist
-node gist-upload script config
-# 删除 config-cf 生成的文件
-rm config/*_free.yaml
-# 提交 github
 git add -A
 git commit -m "Standard Update"
 git push
